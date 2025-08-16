17f5a552eaeca942995453b553dc561a9de9d021
nadavher6 <nadavher6@gmail.com>
2025-08-16 13:05:04 +0300

    good classes and got draw

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from game_class.table import Table
-from game_class.draw  import draw_table
+from game_class.draw  import *
+from const_numbers import *
+
 
 if __name__ == "__main__":
-    # צור שולחן חדש
-    table = Table(length=200, width=100)
+    # # צור שולחן חדש
+    # table = Table(length=TABLE_LENGTH, width=TABLE_WIDTH)
+    #
+    # # הדפס מידע
+    # print("📋 Table size:", table.length, "x", table.width)
+    # print("🕳️ Pockets:", table.pockets)
+    #
+    # print("🎱 Balls on table:")
+    # for ball in table.balls:
+    #     print("   ", ball)
+    #
+    # # צייר את השולחן עם הכדורים
+    # draw_table(table)
+    #draw_random_table()
 
-    # הדפס מידע
-    print("📋 Table size:", table.length, "x", table.width)
-    print("🕳️ Pockets:", table.pockets)
-
-    print("🎱 Balls on table:")
-    for ball in table.balls:
-        print("   ", ball)
-
-    # צייר את השולחן עם הכדורים
-    draw_table(table)
+    draw_random_white_and_black()
