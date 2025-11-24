180206eb17d3f10de577270d63ef74a451747f01
nadavher6 <nadavher6@gmail.com>
2025-11-25 00:13:29 +0200

    begore cut line

--- analyzer_table/black_white_detect/try.py
+++ analyzer_table/black_white_detect/try.py
@@ -1,7 +1,7 @@
 from detect_balls_and_pockets import detect_only_pockets_and_draw , find_corner_pockets_from_mask
 if __name__ == "__main__":
     print ("Starting test for detect_only_pockets_and_draw...")
-    _, out = find_corner_pockets_from_mask('output/debug/final_detected.png')
+    _, out = find_corner_pockets_from_mask('output/debug/cropped_63a578eaa_test3_b2b.jpeg')
 
     print ("Test completed.")
     print (out)
